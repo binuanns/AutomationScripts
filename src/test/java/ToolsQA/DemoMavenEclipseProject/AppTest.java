285217c3784a8e0fc40f43555e717bc0ce567f93
Binu Kuriakose <binu.kuriakose@experionglobal.com>
2018-12-08 11:43:46 +0300

    TestDemo

---
+++ src/test/java/ToolsQA/DemoMavenEclipseProject/AppTest.java
@@ -0,0 +1,13 @@
+package ToolsQA.DemoMavenEclipseProject;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+ {
+       System.out.println("Hi");
+    }
+}
